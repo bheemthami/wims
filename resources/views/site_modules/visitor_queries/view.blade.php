53b72f83e935ba1b5ff68f8e0d7b0217f0e5c503
BKT <bheem.thami@ditagtech.com.au>
2026-01-15 22:05:06 +0545

    refactor: remove unused files - vistor queries feature

--- resources/views/site_modules/visitor_queries/view.blade.php
+++ resources/views/site_modules/visitor_queries/view.blade.php
@@ -1,68 +1,70 @@
 @extends('layouts.admin.app')
 
-@section('title','Visitor Query > View details')
+@section('title', 'Visitor Query > View details')
 
 @section('content')
-<!-- Content Header (Page header) -->
-<section class="content-header">
-	<h1> Details</h1>
-	<ol class="breadcrumb">
-		<li><a href="{{route('dashboard')}}"><i class="fa fa-dashboard"></i> Dashboard</a></li>
-		<li><a href="{{ route('visitor_queries.index') }}">Visitor Queries</a></li>
-		<li class="active">Show</li>
-	</ol>
-</section>
-<!-- Main content -->
-<section class="content">
-	<!-- Default box -->
-	<div class="box">
-		<div class="box-header with-border">
-			<div class="box-tools pull-right">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+        <h1> Details</h1>
+        <ol class="breadcrumb">
+            <li><a href="{{ route('dashboard') }}"><i class="fa fa-dashboard"></i> Dashboard</a></li>
+            <li><a href="{{ route('visitor_queries.index') }}">Visitor Queries</a></li>
+            <li class="active">Show</li>
+        </ol>
+    </section>
+    <!-- Main content -->
+    <section class="content">
+        <!-- Default box -->
+        <div class="box">
+            <div class="box-header with-border">
+                <div class="box-tools pull-right">
+                    <a class="btn btn-sm btn-danger" href="{{ route('visitor_queries.index') }}"><i class="fa fa-times"></i>
+                    </a>
+                </div>
+            </div>
 
-				<a class="btn btn-sm btn-danger" href="{{ route('visitor_queries.index')}}"><i class="fa fa-times"></i></a>
+            <div class="box-body">
+                <fieldset class="fieldset-border">
+                    <legend class="legend-border">Query Details</legend>
+                    <div class="row-auto">
+                        <div class="col-md-12">
+                            <p> <strong> Qid: </strong>{{ $visitor_query->qid }} </p>
+                        </div>
+                        <div class="col-md-12">
+                            <p> <strong> Year: </strong>{{ $visitor_query->academicYear->year }} </p>
+                        </div>
+                        <div class="col-md-12">
+                            <p> <strong> Name:- </strong>{{ $visitor_query->name }} </p>
+                        </div>
 
-				<button type="button" class="btn btn-box-tool" data-widget="collapse" data-toggle="tooltip" title="Collapse">
-					<i class="fa fa-minus"></i></button>
-			</div>
-		</div>
+                        <div class="col-md-12">
+                            <p> <strong> Email: </strong>{{ $visitor_query->email }} </p>
+                        </div>
 
-		<div class="box-body">
-			<fieldset class="fieldset-border">
-				<legend class="legend-border">Details</legend>
-				<div class="col-md-12">
-					<p> <strong> Qid:- </strong>{{$visitor_query->qid}} </p>
-				</div>
-				<div class="col-md-12">
-					<p> <strong> Year:- </strong>{{$visitor_query->academicYear->year}} </p>
-				</div>
-				<div class="col-md-12">
-					<p> <strong> Name:- </strong>{{$visitor_query->name}} </p>
-				</div>
+                        <div class="col-md-12">
+                            <p> <strong> Subject: </strong>{{ $visitor_query->subject }} </p>
+                        </div>
+                        <div class="col-md-12">
+                            <h5> <strong> Message: </strong></h5>
+                            <p>{!! $visitor_query->message !!} </p>
+                        </div>
 
-				<div class="col-md-12">
-					<p> <strong> Email:- </strong>{{$visitor_query->email}} </p>
-				</div>
+                        <div class="col-md-12">
+                            @if ($visitor_query->status == 0)
+                                <label class="btn btn-sm btn-warning">New Query</label>
+                            @else
+                                <label class="btn btn-sm btn-success">Viewed</label>
+                            @endif
 
-				<div class="col-md-12">
-					<p> <strong> Subject:- </strong>{{ $visitor_query->subject }} </p>
-				</div>
-				<div class="col-md-12">
-					<p> <strong> Message:- </strong>{!! $visitor_query->message !!} </p>
-				</div>
-
-				<div class="col-md-12">
-					@if($visitor_query->status == 0)
-					<label class="btn btn-sm btn-warning">New Query</label>
-					@else
-					<label class="btn btn-sm btn-success">Viewed</label>
-					@endif
-
-					<a class="btn btn-sm btn-danger" data-toggle="modal" data-target="#deleteModal" data-id="{{$visitor_query->id}}" data-route="{{route('visitor_queries.destroy', $visitor_query->id) }}"><i class="fa fa-trash"></i> Delete</a>
-				</div>
-
-			</fieldset>
-		</div>
-	</div>
-	<!-- /.box -->
-</section>
+                            <a class="btn btn-sm btn-danger" data-toggle="modal" data-target="#deleteModal"
+                                data-id="{{ $visitor_query->id }}"
+                                data-route="{{ route('visitor_queries.destroy', $visitor_query->id) }}"><i
+                                    class="fa fa-trash"></i> Delete</a>
+                        </div>
+                    </div>
+                </fieldset>
+            </div>
+        </div>
+        <!-- /.box -->
+    </section>
 @endsection
